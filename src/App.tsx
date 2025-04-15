80418c7faf149116c849e838d89347d347caa6c6
Rki0 <pky00823@gmail.com>
2025-04-15 21:24:31 +0900

    feat: add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
 function App() {
-  return <></>;
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const onInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onButtonClickHandler = () => {
+    setTodos((prev) => {
+      return [
+        ...prev,
+        {
+          id: todos.length + 1,
+          text: value,
+        },
+      ];
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <input type="text" value={value} onChange={onInputChangeHandler} />
+
+        <button onClick={onButtonClickHandler}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
